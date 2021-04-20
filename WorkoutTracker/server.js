a23b70870a16c4b69e5a99c38527867fd7c545e2
Nancy McBride <njmcbride224@gmail.com>
2021-04-20 15:25:12 -0500

    adding provided files and router files that I created

---
+++ WorkoutTracker/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/workoutTracker";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+require("./controllers/apiRoutes")(app);
+require("./controllers/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log(`App listening on Port ${PORT}`);
+});
