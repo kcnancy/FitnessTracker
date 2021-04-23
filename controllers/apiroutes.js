cd22726bd0913d6d30c11903f8360cde99a0ef26
Nancy McBride <njmcbride224@gmail.com>
2021-04-23 08:41:12 -0500

    capitalizaiont inconsistency resolved

--- controllers/apiroutes.js
+++ controllers/apiroutes.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = (app) => {
   app.get("/api/workouts", (req, res) => {
-    db.Workout.aggregate([
+    db.workout.aggregate([
       {
         $sort: {
           day: -1,
